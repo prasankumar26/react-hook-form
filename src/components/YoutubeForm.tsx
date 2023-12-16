e159404661897a26499b08088a80c401f3846e9f
Prasan Kumar <90718794+prasankumar26@users.noreply.github.com>
2023-12-16 13:28:42 +0530

    basic validations

---
+++ src/components/YoutubeForm.tsx
@@ -0,0 +1,61 @@
+import { useForm } from "react-hook-form"
+import { DevTool } from "@hookform/devtools"
+
+type FormValues = {
+  username: string,
+  email: string,
+  channel: string,
+}
+
+const YoutubeForm = () => {
+  const form = useForm<FormValues>();
+  const {register, control, handleSubmit, formState} = form; 
+  const {errors} = formState; 
+
+  const onSubmit = (data:FormValues) =>{
+    console.log("form submitted", data);
+    alert(JSON.stringify(data))
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} noValidate>
+   
+   <div className="form-control">
+   <label htmlFor="username">Username</label>
+    <input 
+    type="text" 
+    id="username" 
+    {...register("username", {
+      required: 'Username is required'
+    })}
+    />
+    <p className="error">{errors?.username?.message}</p>
+   </div>
+
+    <div className="form-control">
+    <label htmlFor="email">E-mail</label>
+    <input type="email" id="email" {...register("email", {
+       required: 'Email is required',
+       pattern: {
+         value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+         message: 'Invalid email address'
+       }
+    })} />
+    <p className="error">{errors?.email?.message}</p>
+    </div>
+
+    <div className="form-control">
+    <label htmlFor="channel">Channel</label>
+    <input type="text" id="channel" {...register("channel", {
+       required: 'Username is required'
+    })} />
+    <p className="error">{errors?.channel?.message}</p>
+    </div>
+
+    <button>Submit</button>
+    <DevTool control={control} />
+  </form>
+  )
+}
+
+export default YoutubeForm
